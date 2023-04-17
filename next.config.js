fecd05f12746f767332ca83dd26666559a81d496
VanLamCS <levanlam7602@gmail.com>
2023-04-18 02:51:48 +0700

    Modified env

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
-const { parsed: localEnv } = require("dotenv").config();
+const { parsed: envVars } = require("dotenv").config();
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["cdn.lep.vn"],
   },
   env: {
-    SERVER_HOST: localEnv.SERVER_HOST,
+    ...envVars,
   },
 };
 
