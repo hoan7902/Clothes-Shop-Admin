6adb66e4dd49d6c098497a6158921c8ac10417a6
VanLamCS <levanlam7602@gmail.com>
2023-04-17 18:24:42 +0700

    Modified _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,19 +8,19 @@ import ReloadContext from "@/context/ReloadContext";
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const [reload, setReload] = useState(false);
+    const router = useRouter();
+    const [reload, setReload] = useState(false);
 
-  useEffect(() => {
-    const user = localStorage.getItem("user");
-    console.log("reload");
-    if (!user) {
-      router.push("/");
-    }
-  }, [reload]);
-  return (
-    <ReloadContext.Provider value={{ reload, setReload }}>
-      <Component {...pageProps} />;
-    </ReloadContext.Provider>
-  );
+    useEffect(() => {
+        const user = localStorage.getItem("user");
+        console.log("reload");
+        if (!user) {
+            router.push("/");
+        }
+    }, [reload]);
+    return (
+        <ReloadContext.Provider value={{ reload, setReload }}>
+            <Component {...pageProps} />
+        </ReloadContext.Provider>
+    );
 }
